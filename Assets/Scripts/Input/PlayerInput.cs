872b10c2e334c3824b5e32ab88b90e98062b2a4f
CyberMagic404 <81950751+CyberMagic404@users.noreply.github.com>
2022-11-07 19:37:24 +0500

    Передвижение игрока

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    InputHandler inputs;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private void Awake()
+    {
+        inputs = new InputHandler();
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(inputs.Player.Movement.ReadValue<Vector2>());
+        Move();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Player.Movement.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        inputs.Player.Movement.Disable();
+
+    }
+
+    private void Move()
+    {
+        Vector2 dir = inputs.Player.Movement.ReadValue<Vector2>();
+        transform.Translate(new Vector3(0, 0, dir.y) * moveSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(0, dir.x * rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+
+    
+}
